fa9ea3e7449e0cfe5fa48dfa25a26f8a9910a990
sandraperez93 <>
2015-11-04 20:41:41 +0100

    practica 7

--- src/pr7app/Pr7app.java
+++ src/pr7app/Pr7app.java
@@ -4,7 +4,16 @@
  * and open the template in the editor.
  */
 package pr7app;
-
+import it.sauronsoftware.ftp4j.FTPAbortedException;
+import it.sauronsoftware.ftp4j.FTPClient;
+import it.sauronsoftware.ftp4j.FTPDataTransferException;
+import it.sauronsoftware.ftp4j.FTPException;
+import it.sauronsoftware.ftp4j.FTPIllegalReplyException;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 /**
  *
  * @author ivan
@@ -14,11 +23,30 @@ public class Pr7app {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException, FTPException, FTPIllegalReplyException, IllegalStateException, FileNotFoundException, FTPDataTransferException, FTPAbortedException {
+ 
+        URL url = new URL("http://192.168.122.200/docs/ic10-m04-WindowsServer.pdf");
+        url.openConnection();
+        InputStream reader = url.openStream();
+        FileOutputStream writer = new FileOutputStream("sandra.pdf");
+        byte[] buffer = new byte[153600];
+        int bytesRead = 0;
+        while ((bytesRead = reader.read(buffer)) > 0)
+        {  
+            writer.write(buffer, 0, bytesRead);
+        }
+        writer.close();
+        reader.close();
         
-        System.out.println("Hola profes!");
-        System.out.println("Hola!");
+        FTPClient client = new FTPClient();
+        client.connect("srv.toca.cat");
+        client.login("fulano", "Platano123");
+        client.upload(new java.io.File("sandra.pdf"));
+        
+        
+        
+        client.disconnect(true);
+        
     }
     
 }
